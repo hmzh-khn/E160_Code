ea713f339b45fd171b8e1a90e917c760fb0561d1
Loaner <Loaner@WL-209-206.WPA.Claremont.Edu>
2018-03-19 23:36:41 -0700

    robot ground truth is shown

--- E160_graphics.py
+++ E160_graphics.py
@@ -211,9 +211,9 @@ class E160_graphics:
     def draw_robot(self, robot):
         
         # gif update
-        robot.tkimage = ImageTk.PhotoImage(robot.robot_gif.rotate(180/3.14*robot.state_est.theta))
-        robot.image = self.canvas.create_image(robot.state_est.x, robot.state_est.y, image=robot.tkimage)
-        robot_points = self.scale_points([robot.state_est.x, robot.state_est.y], self.scale)
+        robot.tkimage = ImageTk.PhotoImage(robot.robot_gif.rotate(180/3.14*robot.state_draw.theta))
+        robot.image = self.canvas.create_image(robot.state_draw.x, robot.state_draw.y, image=robot.tkimage)
+        robot_points = self.scale_points([robot.state_draw.x, robot.state_draw.y], self.scale)
         self.canvas.coords(robot.image, *robot_points)
             
     def get_inputs(self):
