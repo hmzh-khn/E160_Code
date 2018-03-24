8e49c12b4a42ee6ad206d42b04dff926dd86d329
Loaner <Loaner@WL-220-240.WPA.Claremont.Edu>
2018-03-23 20:20:55 -0700

    fixed walls not existing, match out temp course, wall intersect at vertical

--- E160_environment.py
+++ E160_environment.py
@@ -12,14 +12,17 @@ class E160_environment:
     def __init__(self):
         self.width = 2.0
         self.height = 2.0 # was 2.0 in old code, change for lab 3
-        
+        in2m = 0.0254 
         # set up walls, putting top left point first
         self.walls = []
-        self.walls.append(E160_wall([-0.5, 0.5, -0.5, -0.5],"vertical"))
-        self.walls.append(E160_wall([0.5, 0.5, 0.5, -0.5],"vertical"))
-        self.walls.append(E160_wall([-0.5, 0.5, 0.5, 0.5],"horizontal"))
-        self.walls.append(E160_wall([0.5, -0.5, 1.0, -0.5],"horizontal"))
-        self.walls.append(E160_wall([0.0, -0.5, 0.0, -1.0],"vertical"))
+        self.walls.append(E160_wall([x*in2m for x in [-19, 8.5, -19, -10.5]],"vertical"))
+        self.walls.append(E160_wall([x*in2m for x in [-19, 8.5, -22, 8.5]],"horizontal"))
+        self.walls.append(E160_wall([x*in2m for x in [-22, 22.5, -22, 8.5]],"vertical"))
+        self.walls.append(E160_wall([x*in2m for x in [-3, 50.5, 7, 50.5]],"horizontal"))
+        self.walls.append(E160_wall([x*in2m for x in [19.5, 7.5, 19.5, -10.5]],"vertical"))
+        self.walls.append(E160_wall([x*in2m for x in [-19, -10.5, 19.5, -10.5]],"horizontal"))
+
+        self.walls.append(E160_wall([x*in2m for x in [19.5, 7.5, 25.5, 23.5]],"horizontal"))
             
         # create vars for hardware vs simulation
         self.robot_mode = CONFIG_ROBOT_MODE
